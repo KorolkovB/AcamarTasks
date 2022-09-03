76e2ba8d53f6065b2f3959969c35f57658036581
Корольков Виктор Валерьевич <bilyal.korolkov@gmail.com>
2022-09-03 19:22:49 +0300

    - Task1 and Task2 for Acamar

---
+++ src/main/java/cz/acamar/tasks/Task1.java
@@ -0,0 +1,34 @@
+package cz.acamar.tasks;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Task1 {
+
+    /**
+     * Task 1. Length of the last word.
+     * <p>
+     * Given a string s consisting of some words separated by some number of spaces,
+     * return the length of the last word in the string.
+     * <p>
+     * A word is a maximal substring consisting of non-space characters only.
+     * <p>
+     * Example input: "Hello World"
+     * Expected output: 5
+     *
+     * @param str - a string s consisting of some words separated by some number of spaces.
+     * @return - the length of the last word in the string.
+     */
+    public int lengthOfLastWord(String str) {
+        try {
+            List<String> words = Arrays.stream(str.split(" "))
+                    .filter(word -> word.matches("\\S*"))
+                    .collect(Collectors.toList());
+
+            return words.get(words.size() - 1).length();
+        } catch (NullPointerException | IndexOutOfBoundsException ex) {
+            return 0;
+        }
+    }
+}
